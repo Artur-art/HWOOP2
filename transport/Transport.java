f1a1f7616e4e90c60d6264a3529e53cf9b88ebc9
archy <archytigerman93@outlook.com>
2023-01-05 04:55:55 +0300

    ДЗ №4

--- transport/Transport.java
+++ transport/Transport.java
@@ -5,13 +5,13 @@ import transport.transports.Bus;
 import transport.transports.Car;
 import transport.transports.Truck;
 import utils.Utils;
+
 public abstract class Transport implements Competing {
     private final String type;
     protected final String brand;
     protected final String model;
-
     private double engineVolume;
-    private Driver driver;
+    protected Driver driver;
 
     public Transport(String brand, String model, double engineVolume, Driver driver) {
         if (this instanceof Car) {
@@ -23,71 +23,88 @@ public abstract class Transport implements Competing {
         } else {
             type = "default";
         }
+
         if (Utils.isStringNotNullAndNotBlank(brand)) {
             this.brand = brand;
         } else {
             this.brand = "default";
         }
+
         if (Utils.isStringNotNullAndNotBlank(model)) {
             this.model = model;
         } else {
             this.model = "default";
         }
+
         setEngineVolume(engineVolume);
+
         this.driver = driver;
     }
 
     public String getBrand() {
         return brand;
     }
+
     public String getModel() {
         return model;
     }
-        public double getEngineVolume() {
-            return engineVolume;
-        }
-        public Driver getDriver() {
-            return driver;
-        }
-        public void setEngineVolume(double engineVolume) {
-            if (engineVolume > 0) {
-                this.engineVolume = engineVolume;
-            } else {
-                this.engineVolume = 1.5;
-            }
-        }
-        public void setDriver(Driver driver) {
-            this.driver = driver;
-        }
-        public void startMoving() {
-            System.out.println(type + ' ' + brand + " " + model + " начал движение.");
-        }
-        public void endMoving() {
-            System.out.println(type + ' ' + brand + " " + model + " закончил движение.");
-        }
-        public abstract void printType();
 
-        @Override
-        public void pitStop() {
-            System.out.println(type + ' ' + brand + " " + model + " на пит-стопе.");
-        }
+    public double getEngineVolume() {
+        return engineVolume;
+    }
 
-        @Override
-        public void bestLapTime() {
-            System.out.println("Лучший круг " + brand + " " + model + ": " + 5 + 3 * Math.random() + " мин.");
-        }
+    public Driver getDriver() {
+        return driver;
+    }
 
-        @Override
-        public void maxSpeed() {
-            System.out.println("Максимальная скорость " + brand + " " + model + ": " + 5 + 3 * Math.random() + " км/ч.");
-        }
-
-        @Override
-        public String toString() {
-            return "========================================\n" +
-                    "[" + type + "] " + brand + " " + model + '\n'
-                    + "Мощность двигателя: " + engineVolume + '\n'
-                    + (driver != null ? "Водитель: " + driver + '\n' : "")
-                    + "========================================";
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume > 0) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = 1.5;
         }
     }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public void startMoving() {
+        System.out.println(type + ' ' + brand + " " + model + " начал движение.");
+    }
+
+    public void endMoving() {
+        System.out.println(type + ' ' + brand + " " + model + " закончил движение.");
+    }
+
+    public abstract void printType();
+
+    // -1 – для транспортных средств, не требующих диагностики
+    // 0 – для непрошедших диагностику транспортных средств
+    // 1 – для прошедших диагностику транспортных средств
+    public abstract int getDiagnostic();
+
+    @Override
+    public void pitStop() {
+        System.out.println(type + ' ' + brand + " " + model + " на пит-стопе.");
+    }
+
+    @Override
+    public void bestLapTime() {
+        System.out.println("Лучший круг " + brand + " " + model + ": " + 5 + 3 * Math.random() + " мин.");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("Максимальная скорость " + brand + " " + model + ": " + 5 + 3 * Math.random() + " км/ч.");
+    }
+
+    @Override
+    public String toString() {
+        return "========================================\n" +
+                "[" + type + "] " + brand + " " + model + '\n'
+                + "Мощность двигателя: " + engineVolume + '\n'
+                + (driver != null ? "Водитель: " + driver + '\n' : "")
+                + "========================================";
+    }
+}
